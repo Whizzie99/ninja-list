ff51079274477fe8e3a08738edba78f57f6b703e
Whizzie99 <47413415+Whizzie99@users.noreply.github.com>
2021-07-17 02:28:02 +0100

    first commit

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About </title>
+                <meta name="keywords" content="ninjas"/>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae modi assumenda voluptate enim laboriosam sequi dolor error deleniti, ut recusandae commodi mollitia quibusdam voluptatem quisquam id praesentium eligendi eos non.
+                </p>
+            </div>
+        </>
+    );
+}
+ 
+export default About;
